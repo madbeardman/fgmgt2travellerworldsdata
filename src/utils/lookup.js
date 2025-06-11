b90e12c8ea8dd13e483ea3587525413155a395b9
Colin Richardson <colinkrichardson@yahoo.com>
2025-06-11 12:45:07 +0100

    Fixed up the lookup codes

--- src/utils/lookup.js
+++ src/utils/lookup.js
@@ -1,31 +1,129 @@
 export const TRADE_CODES = {
-  "Ag": "Agricultural",
-  "As": "Asteroid",
-  "Ba": "Barren",
-  "De": "Desert",
-  "Fl": "Fluid Oceans",
-  "Ga": "Garden",
-  "Hi": "High Population",
-  "Ht": "High Tech",
-  "Ic": "Ice-Capped",
-  "In": "Industrial",
-  "Lo": "Low Population",
-  "Lt": "Low Tech",
-  "Na": "Non-Agricultural",
-  "Ni": "Non-Industrial",
-  "Po": "Poor",
-  "Ri": "Rich",
-  "Va": "Vacuum",
-  "Wa": "Water World"
+  Ag: "Agricultural",
+  As: "Asteroid",
+  Ba: "Barren",
+  De: "Desert",
+  Fl: "Fluid Oceans",
+  Ga: "Garden",
+  Hi: "High Population",
+  Ht: "High Tech",
+  Ic: "Ice-Capped",
+  In: "Industrial",
+  Lo: "Low Population",
+  Lt: "Low Tech",
+  Na: "Non-Agricultural",
+  Ni: "Non-Industrial",
+  Po: "Poor",
+  Ri: "Rich",
+  Va: "Vacuum",
+  Wa: "Water World"
 };
 
 export const STARPORT_QUALITY = {
-  "A": "Excellent",
-  "B": "Good",
-  "C": "Routine",
-  "D": "Poor",
-  "E": "Frontier",
-  "X": "No Starport"
+  A: "Excellent",
+  B: "Good",
+  C: "Routine",
+  D: "Poor",
+  E: "Frontier",
+  X: "No Starport"
 };
 
-// (Other lookup tables omitted here for brevity)
+export const SIZE = {
+  "1": "1,600km",
+  "2": "3,200km, Triton, Luna, Europa",
+  "3": "4,800km, Mercury, Ganymede",
+  "4": "6,400km, Mars",
+  "5": "8,000km",
+  "6": "9,600km",
+  "7": "11,200km",
+  "8": "12,800km Earth",
+  "9": "14,400km",
+  A: "16,000km"
+};
+
+export const ATMOSPHERE_TYPE = {
+  "0": "None",
+  "1": "Trace",
+  "2": "Very Thin, Tainted",
+  "3": "Very Thin",
+  "4": "Thin, Tainted",
+  "5": "Thin",
+  "6": "Standard",
+  "7": "Standard, Tainted",
+  "8": "Dense",
+  "9": "Dense, Tainted",
+  A: "Exotic",
+  B: "Corrosive",
+  C: "Insidious",
+  D: "Dense, High",
+  E: "Thin, Low",
+  F: "Unusual"
+};
+
+export const HYDROGRAPHICS = {
+  "0": "0%-5% Desert world",
+  "1": "6%-15% Dry world",
+  "2": "16%-25% A few small seas",
+  "3": "26%-35% Small seas and oceans",
+  "4": "36%-45% Wet world",
+  "5": "46%-55% Large oceans",
+  "6": "56%-65%",
+  "7": "66%-75% Earth-like world",
+  "8": "76%-85% Water world",
+  "9": "86%-95% Only a few small islands and archipelagos",
+  A: "96-100% Almost entirely water"
+};
+
+export const POPULATION = {
+  "0": "None",
+  "1": "Few",
+  "2": "Hundreds",
+  "3": "Thousands",
+  "4": "Tens of Thousands",
+  "5": "Hundreds of thousands",
+  "6": "Millions",
+  "7": "Tens of millions",
+  "8": "Hundreds of millions",
+  "9": "Billions",
+  A: "Tens of billions",
+  B: "Hundreds of billions",
+  C: "Trillions"
+};
+
+export const GOVERNMENT_TYPE = {
+  "0": "None",
+  "1": "Company/corporation",
+  "2": "Participating democracy",
+  "3": "Self-perpetuating oligarchy",
+  "4": "Representative democracy",
+  "5": "Feudal technocracy",
+  "6": "Captive government",
+  "7": "Balkanisation",
+  "8": "Civil service bureaucracy",
+  "9": "Impersonal Bureaucracy",
+  A: "Charismatic dictator",
+  B: "Non-charismatic leader",
+  C: "Charismatic oligarchy",
+  D: "Religious dictatorship"
+};
+
+export const LAW_LEVEL = {
+  "0": "No restrictions - heavy armour and a handy weapon recommended...",
+  "1": "No Poison gas, explosives, undetectable weapons, WMD or Battle Dress",
+  "2": "No Portable energy and laser weapons or Combat armour",
+  "3": "No Military weapons or Flak armour",
+  "4": "No Light assault weapons and submachine guns or Cloth armour",
+  "5": "No Personal concealable weapons or Mesh armour",
+  "6": "No firearms except shotguns & stunners; carrying weapons discouraged",
+  "7": "No Shotguns",
+  "8": "No bladed weapons, stunners or visible armour",
+  "9": "No weapons, No armour",
+  A: "No weapons, No armour",
+  B: "No weapons, No armour",
+  C: "No weapons, No armour",
+  D: "No weapons, No armour",
+  E: "No weapons, No armour",
+  F: "No weapons, No armour",
+  G: "No weapons, No armour",
+  H: "No weapons, No armour"
+};
